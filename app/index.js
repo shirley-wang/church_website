247a0f8d3e5d230dafb04543e8db7e1c74014c88
shirley-wang <xuanli512@gmail.com>
2016-06-28 22:59:16 -0400

    nav bar added
    Fixed Navigation bar added

--- app/index.js
+++ app/index.js
@@ -5,7 +5,37 @@ var App = React.createClass({
   render: function(){
     return (
       <div>
-        <h1>This is the church website</h1>
+        <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
+          <div className="container">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <a className="navbar-brand" href="#"></a>
+            </div>
+            <div id="navbar" className="navbar-collapse collapse">
+              <ul className="nav navbar-nav">
+                <li className="active"><a href="#">Home</a></li>
+                <li><a href="#">Articles</a></li>
+                <li><a href="#">Core Beliefs</a></li>
+                <li><a href="#">Our Ministries</a></li>
+                <li><a href="#">Our Objectives As We Meet</a></li>
+                <li><a href="#">Rides to Church</a></li>
+                <li><a href="#">Sermons</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className="jumbotron">
+          <h1>This is the church website</h1>
+        </div>
+        <div className="container">
+        </div>
+        <footer id="contact" className="row-footer">
+        </footer>
       </div>
     );
   }
