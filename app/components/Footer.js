3657e3a712fc4daa25e43cf57e736c3953bc2e9b
danieljit2001 <danieljit2001@gmail.com>
2016-06-28 22:22:05 -0700

    update
    update with menu

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -25,20 +25,6 @@ module.exports = React.createClass({
 					</div>
 				</div>
 			</div>
-/*			<div className="container">
-					<div className="row-fluid">		
-						<div class="span12">	
-						<div className="span8">
-							<a href="#">Terms of Service</a>    
-							<a href="#">Privacy</a>    
-							<a href="#">Security</a>
-						</div>
-						<div className="span4">
-							<p className="muted pull-right">© 2016 Providence Bible Church. All rights reserved</p>
-						</div>	
-						</div>
-					</div>
-			</div>*/
 			);
 	}
 });
